153498aefbab4b7c5d849faaab780951abfee25a
Charnay Birton <coding26fig@gmail.com>
2022-04-06 20:38:07 -0400

    final

--- js/script.js
+++ js/script.js
@@ -8,22 +8,71 @@ function currentDate(){
 currentDate();
 
 
+function initialize(){
+    let storageArr = JSON.parse(localStorage.getItem('userInput'))
+    let textAreas = document.querySelectorAll('.description')
+    let container = document.querySelector('.container')
 
-for(let i = 0; i < saveButton.length; i++){
-    saveButton[i].addEventListener("click",(event) => {
-        save(event)
-    })
+    for(let i = 0; i < saveButton.length; i++){
+
+        saveButton[i].addEventListener("click",(event) => {
+            save(event)
+        })
+
+        textAreas[i].value = storageArr[i]
+
+
+    }
+
+    let date = new Date()
+
+    let hours = date.getHours()
+
+    for(let i = 0; i < 9; i++){
+        if(parseInt(container.children[i].id) < hours){
+            container.children[i].classList.add('past')
+        }else if(parseInt(container.children[i].id) === hours){
+            container.children[i].classList.add('present')
+        }else{
+            container.children[i].classList.add('future')
+        }
+    }
+
+
+    console.log(hours)
+
+    console.log(date)
+
+    // let arr = []
+    // for(let i = 0; i < 9; i++){
+    //     arr.push('')
+    // }
+   
+
+
+    // localStorage.setItem("userInput", JSON.stringify(arr))
 }
 
+initialize()
+
 function save(event){
+    let id =  event.target.parentNode.parentNode.id
+    let container = event.target.parentNode.parentNode.parentNode
     let textarea = event.target.parentNode.parentNode.children[1].value
-    console.log(textarea)
-    localStorage.setItem("userInput", textarea)
+
+    for(let i = 0; i < container.children.length; i++){
+        if(id === container.children[i].id){
+            let storageArr = JSON.parse(localStorage.getItem('userInput'))
+            storageArr[i] = textarea
+            localStorage.setItem("userInput", JSON.stringify(storageArr))
+        }
+    }
+    
     console.log(localStorage.getItem("userInput"))
 }
 
 
-console.log(localStorage.getItem('userInput'))
+
 
 
 
